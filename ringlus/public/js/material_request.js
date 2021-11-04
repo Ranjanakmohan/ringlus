c094347d64cca21219e279a97548060a961cc78d
jiloysss <janlloydangeles@gmail.com>
2021-11-04 12:50:23 +0800

    Merge branch 'main' of https://github.com/Momscode-Technologies/ringlus into main
    # Conflicts:
    #	ringlus/ringlus/workspace/ringlus/ringlus.json

--- ringlus/public/js/material_request.js
+++ ringlus/public/js/material_request.js
@@ -7,18 +7,20 @@ frappe.ui.form.on("Budget BOM References", {
                 .then(doc => {
                     var fields = ["electrical_bom_raw_material","mechanical_bom_raw_material","fg_sellable_bom_raw_material",]
                 for(var x=0;x<fields.length;x+=1){
-                    for(var i=0;i<doc[fields[x]].length;i+=1){
-                        cur_frm.add_child("items",{
-                            item_code: doc[fields[x]][i].item_code,
-                            item_name: doc[fields[x]][i].item_name,
-                            description: doc[fields[x]][i].item_name,
-                            qty: doc[fields[x]][i].qty,
-                            uom: doc[fields[x]][i].uom,
-                            warehouse: doc[fields[x]][i].warehouse,
-                            rate: doc[fields[x]][i].rate,
-                            budget_bom_rate: doc[fields[x]][i].rate,
-                        })
-                        cur_frm.refresh_field("items")
+                    for(var i=0;i<doc[fields[x]].length;i+=1) {
+                        if (!check_items(doc[fields[x]][i], cur_frm)) {
+                            cur_frm.add_child("items", {
+                                item_code: doc[fields[x]][i].item_code,
+                                item_name: doc[fields[x]][i].item_name,
+                                description: doc[fields[x]][i].item_name,
+                                qty: doc[fields[x]][i].qty,
+                                uom: doc[fields[x]][i].uom,
+                                warehouse: doc[fields[x]][i].warehouse,
+                                rate: doc[fields[x]][i].rate,
+                                budget_bom_rate: doc[fields[x]][i].rate,
+                            })
+                            cur_frm.refresh_field("items")
+                        }
                     }
                 }
 
