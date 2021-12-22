34b669bebc5d7e01120c7ca701f272e9e3319b30
jiloysss <janlloydangeles@gmail.com>
2021-12-22 09:59:24 +0800

    Merge branch 'main' of https://github.com/Momscode-Technologies/ringlus into main
    # Conflicts:
    #	ringlus/ringlus/workspace/ringlus/ringlus.json

--- ringlus/ringlus/doctype/modular_component/modular_component.py
+++ ringlus/ringlus/doctype/modular_component/modular_component.py
@@ -1,9 +1,25 @@
 # Copyright (c) 2021, jan and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class ModularComponent(Document):
 	def autoname(self):
 		self.name = self.items[0].item_name
+
+	def validate(self):
+		for i in self.raw_materials:
+			if i.uom:
+				i.conversion_factor = self.get_conversion_factor(i.item_code, i.uom)
+
+	@frappe.whitelist()
+	def get_conversion_factor(self, item_code, uoms):
+		uom = frappe.db.sql(""" SELECT * FROm `tabUOM Conversion Detail` WHERE parent=%s and uom=%s""",
+							(item_code, uoms), as_dict=1)
+
+		if len(uom) > 0:
+			return uom[0].conversion_factor
+
+		return 1
+
