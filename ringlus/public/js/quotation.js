c5f1b0483c02be9f83c1384114a29c06ff03a8d9
jiloysss <janlloydangeles@gmail.com>
2022-01-03 10:45:54 +0800

    Merge branch 'main' of https://github.com/Momscode-Technologies/ringlus into main
    # Conflicts:
    #	ringlus/ringlus/workspace/ringlus/ringlus.json

--- ringlus/public/js/quotation.js
+++ ringlus/public/js/quotation.js
@@ -185,7 +185,7 @@ function fetch_boms(cur_frm, selections) {
 
                         for(var ii=0;ii<doc.fg_bom_details.length;ii+=1){
                                 var material_cost = doc.total_raw_material_cost + (doc.total_raw_material_cost * (cur_frm.doc.default_material_overhead / 100 ))
-                                var operation_cost = (doc.total_operation_cost + doc.total_additional_operational_cost) + ((doc.total_operation_cost + doc.total_additional_operational_cost) *(cur_frm.doc.default_operation_overhead / 100 ))
+                                var operation_cost = (doc.total_operation_cost + doc.total_additional_operational_cost) + ((doc.total_operation_cost + doc.total_additional_operational_cost) * (cur_frm.doc.default_operation_overhead / 100 ))
                                   cur_frm.add_child("items",{
                                         "description": doc.fg_bom_details[ii].item_name + "<br>RFQ Si No - " + doc.fg_bom_details[ii].rfq_si_no + "<br>" + "Product Description - " + doc.fg_bom_details[ii].product_description,
                                         "item_code": doc.fg_bom_details[ii].item_code,
@@ -200,7 +200,7 @@ function fetch_boms(cur_frm, selections) {
 
                                         "estimated_bom_operation_cost": doc.total_operation_cost + doc.total_additional_operational_cost,
                                         "operation_overhead": cur_frm.doc.default_operation_overhead,
-                                        "operation_overhead_amount": doc.total_operation_cost * (cur_frm.doc.default_operation_overhead / 100 ),
+                                        "operation_overhead_amount": (doc.total_operation_cost + doc.total_additional_operational_cost) * (cur_frm.doc.default_operation_overhead / 100 ),
                                         "operation_cost": operation_cost,
 
                                         "material_margin": cur_frm.doc.default_material_margin,
